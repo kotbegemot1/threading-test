fa7b3b58483ef6499c0eec057afdc26001564251
kotbegemot1 <kk1slorod@gmail.com>
2020-04-02 11:40:18 +0300

    Deploy

--- test_mail/settings.py
+++ test_mail/settings.py
@@ -126,9 +126,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
