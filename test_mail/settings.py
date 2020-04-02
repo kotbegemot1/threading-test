4d349258e0ad91d105cdc2aa7135adb761ad2bd4
kotbegemot1 <kk1slorod@gmail.com>
2020-04-02 12:48:33 +0300

    Update

--- test_mail/settings.py
+++ test_mail/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = '8ooh1k(wa5hhel@&9%yt4)r21#78h4k12cx8b6rj4@h5#f8d++'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
