f33ec34f7429a6494a4641eceac63bf2cf3ba716
kotbegemot <kk1slorod@gmail.com>
2021-08-18 17:46:58 +0300

    heroku deploy

--- test_mail/settings.py
+++ test_mail/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "#$%&N(ASFGAD^*(%326n26835625BEWSRTSER&^@T#%$Bwertb")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
